4ff4d0b1c2805918b61bcda912a3d44ead4ec417
Norbert <szilvasi.norbert@gmail.com>
2022-09-10 09:39:44 +0200

    update

--- src/main/java/app/controllers/SpaceShipController.java
+++ src/main/java/app/controllers/SpaceShipController.java
@@ -2,12 +2,22 @@ package app.controllers;
 
 import app.models.SpaceShip;
 import app.models.SpaceShipClass;
+import app.services.SpaceShipService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SpaceShipController {
+
+    //@Autowired
+    private SpaceShipService spaceShipService;
+
+    public SpaceShipController(SpaceShipService spaceShipService) {
+        this.spaceShipService = spaceShipService;
+    }
 
     @GetMapping(value = {"/spaceship"})
     public String saveNewSpaceShip(Model model) {
@@ -15,6 +25,22 @@ public class SpaceShipController {
         model.addAttribute("shipClasses", SpaceShipClass.values());
 
         return "spaceship_new";
+    }
+
+    @GetMapping("/list-spaceship")
+    public String listSpaceShips(Model model, @RequestParam(required = false, name = "isActive") String filter) {
+
+        if (filter == null || filter.equals("")) {
+            model.addAttribute("spaceships", spaceShipService.listAll());
+            model.addAttribute("filtered", "false");
+
+        } else if (filter.equals("true")) {
+            System.out.println(spaceShipService.listAllActive());
+            model.addAttribute("spaceships", spaceShipService.listAllActive());
+            model.addAttribute("filtered", "true");
+        }
+
+        return "list_spaceship";
     }
 
 }
--- src/main/java/app/repositories/SpaceShipRepo.java
+++ src/main/java/app/repositories/SpaceShipRepo.java
@@ -3,5 +3,13 @@ package app.repositories;
 import app.models.SpaceShip;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface SpaceShipRepo extends CrudRepository<SpaceShip, Long> {
+
+    List<SpaceShip> findAll();
+    List<SpaceShip> findAllByIsActiveTrue();
+    Optional<SpaceShip> findByRegistrationCode(Long registrationCode);
+
 }
